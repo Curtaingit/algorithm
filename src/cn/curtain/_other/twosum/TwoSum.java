c1b5eb80ae8f1bb38cddc97ed3d1613fc7d934cf
Cutrian <1158380326@qq.com>
2019-01-11 16:27:12 +0800

    获取数组中两个数和等于结果值的下标 复杂度O(N)

--- src/cn/curtain/_other/twosum/TwoSum.java
+++ src/cn/curtain/_other/twosum/TwoSum.java
@@ -1,5 +1,8 @@
 package cn.curtain._other.twosum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 从数组中返回两个值相加等于目标值的下标
  *
@@ -10,6 +13,7 @@ public class TwoSum {
 
     /**
      * 时间复杂度O(n^2)
+     *
      * @param nums
      * @param target
      * @return
@@ -30,4 +34,26 @@ public class TwoSum {
         return null;
     }
 
+    /**
+     * 时间复杂度O（N）
+     * 利用Map Key存放目标值（当前的差值）Value就是当前的下标
+     * 每次遍历是 查看当前遍历的值是否等于差值，如果等于，说明相加就等于目标值
+     * 然后取出map中的value,和本次遍历的小标就是结果了。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] getTwo2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>(2);
+        for (int i = 0; i < nums.length; i++) {
+            //遍历当前值  是否是组成需要的差值。
+            if (map.containsKey(nums[i])) {
+                result = new int[]{map.get(nums[i]), i};
+
+            }
+            map.put(target-nums[i],i);
+        }
+        return result;
+    }
 }
